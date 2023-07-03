3770bbc674b4f9aa142b2f5674a5d0cce2f562a2
Ijal Fauzi <ijaliffauzi@gmail.com>
2023-07-03 16:21:45 +0700

    Updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#294295",
+        secondary: "#c6002d",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgb(41, 66, 149, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
-}
-
+};
